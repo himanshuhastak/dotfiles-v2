51db16a8c90a65ac4714a6c872cc7f80cce0ffae
Hastak <hhastak@gfoundries.com>
2026-06-23 12:33:57 +0530

    updates

--- config/shell/conf.d/rg.sh
+++ config/shell/conf.d/rg.sh
@@ -4,6 +4,9 @@ command -v rg >/dev/null 2>&1 || return 0
 alias grep='rg --smart-case'
 alias rgi='rg -i'
 
-# Point ripgrep at a user config file (~/.config/ripgrep/ripgreprc) so flags
-# like --hidden, --follow, --glob can be set without typing them every time.
-export RIPGREP_CONFIG_PATH="${RIPGREP_CONFIG_PATH:-${XDG_CONFIG_HOME:-$HOME/.config}/ripgrep/ripgreprc}"
+# Point ripgrep at ~/.config/ripgrep/ripgreprc when stowed (avoid errors if missing).
+_rg_cfg="${RIPGREP_CONFIG_PATH:-${XDG_CONFIG_HOME:-$HOME/.config}/ripgrep/ripgreprc}"
+if [ -r "$_rg_cfg" ]; then
+  export RIPGREP_CONFIG_PATH="$_rg_cfg"
+fi
+unset _rg_cfg
